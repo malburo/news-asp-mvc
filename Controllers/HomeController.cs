bc5db17aef6ae092a10c8b132f65b805ffa364c5
Vũ Hoàng Long <vuhoanglong21@gmail.com>
2021-07-04 20:03:22 +0700

    Showing Category and SubCategory page
    80% UI, 0% pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using NewsApplication.Models;
+using NewsApplication.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,16 +15,61 @@ namespace NewsApplication.Controllers
             return View();
         }
 
-        public ActionResult Categories()
+        public ActionResult Categories(int id)
         {
 
-            return View();
+            ApplicationDbContext db = new ApplicationDbContext();
+            Category category = db.Categories.FirstOrDefault(p => p.CategoryId == id);
+            if(category == null)
+            {
+                HttpNotFound();
+            }
+
+            List < SubCategory > subCategories = db.SubCategories.Where(p => p.CategoryId == id).ToList();
+            List<Post> categoryPosts = (from p in db.Posts 
+                               where p.SubCategory.Any(s => s.CategoryId == id)
+                               orderby p.CreatedAt descending
+                               select p).Take(6).ToList();
+            List<Post> subCategoryPosts = (from p in db.Posts
+                                           where p.SubCategory.Any(s=> s.CategoryId == id)
+                                           orderby p.CreatedAt descending
+                                           select p).ToList();
+            ViewBag.SubCategories = subCategories;
+            ViewBag.CategoryPosts = categoryPosts;
+            ViewBag.SubCategoryPosts = subCategoryPosts;
+            return View(category);
         }
 
-        public ActionResult DetailsCategory()
-        {
 
-            return View();
+        [ChildActionOnly]
+        public ActionResult RenderTop3Posts(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<Post> Posts = (from p in db.Posts
+                                        where p.SubCategory.Any(s => s.SubCategoryId == id)
+                                        orderby p.CreatedAt descending
+                                        select p).Take(3).ToList();
+            ViewBag.Posts = Posts;
+            return PartialView("_Top_3_Posts");
+        }
+
+
+
+        public ActionResult DetailsCategory(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            SubCategory subCategory = db.SubCategories.FirstOrDefault(s => s.SubCategoryId == id);
+            if (subCategory == null)
+            {
+                HttpNotFound();
+            }
+            List<Post> Posts = (from p in db.Posts
+                                           where p.SubCategory.Any(s => s.SubCategoryId == id)
+                                           orderby p.CreatedAt descending
+                                           select p).ToList();
+            ViewBag.Posts = Posts;
+
+            return View(subCategory);
         }
         public ActionResult Details()
         {
--- ViewModels/CategoryViewModels.cs
+++ ViewModels/CategoryViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,6 @@ namespace NewsApplication.ViewModels
 {
     public class CategoryViewModels
     {
+       
     }
 }

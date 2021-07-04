51db5699770b87bb2919cda10fc75451ae3b834a
= <=luucuong446@gmail.com>
2021-07-04 21:18:26 +0700

    sp

---
+++ Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using NewsApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsApplication.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult showPost( int id)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            Post dbpost = context.Posts.FirstOrDefault(p => p.PostId==id);
+            return View(dbpost);
+        }
+
+        public ActionResult relatePost()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            
+            return View();
+        }
+
+    }
+}

c4ec5e5ce98eb2631955a08f416f9f6e8d8a3248
malburo <tongquocbao0605@gmail.com>
2021-07-11 10:16:00 +0700

    merge cuong to main branch

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -11,15 +11,44 @@ namespace NewsApplication.Controllers
     public class PostController : Controller
     {
         private ApplicationDbContext context = new ApplicationDbContext();
-        // GET: Post
+
         public ActionResult showPost(int id)
         {
             Post dbpost = context.Posts.FirstOrDefault(p => p.PostId == id);
             return View(dbpost);
         }
 
-        public ActionResult relatePost()
+        [ChildActionOnly]
+        public ActionResult rate3PostPartial(int id)
         {
+            var posts = (from p in context.Posts orderby p.PostId descending select p).Take(3).ToList();
+            ViewBag.posts = posts;
+            return PartialView("_rate3postPartial");
+
+        }
+        [ChildActionOnly]
+        public ActionResult new3PostPartial(int id)
+        {
+            var posts = (from p in context.Posts orderby p.CreatedAt descending select p).Take(3).ToList();
+            ViewBag.post = posts;
+            return PartialView("_new3PostPartial");
+        }
+
+        [ChildActionOnly]
+        public ActionResult relatedPostPartial(int id)
+        {
+            var listsub = context.Posts.Find(id).SubCategory.First();
+            var posts = from p in context.Posts
+                        where p.SubCategory.Any(s => s.SubCategoryId == listsub.CategoryId)
+                        select p;
+            ViewBag.Posts = posts.OrderByDescending(p => p.CreatedAt).Take(3).ToList();
+            return PartialView("_relatedPostPartial");
+        }
+
+        public ActionResult Search(string searchString)
+        {
+            var search = context.Posts.SqlQuery("Select * from Post Where Title like '%" + searchString + "%'").ToList();
+            ViewBag.Search = search;
             return View();
         }
     }
